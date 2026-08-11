094f3d4be25d48566eb92875c64b4d67d0f2b673
lyx0z <mykyta.kucher@digitecgalaxus.ch>
2026-08-11 10:14:50 +0200

    Datentypen und Variablen

---
+++ ConsoleApp1/PiAufgabe/Program.cs
@@ -0,0 +1,14 @@
+﻿
+double pi = Math.PI;
+double e = Math.E;
+
+float fpi = (float)Math.PI;
+float fe = (float)Math.E;
+
+double RoundedPi = Math.Round(pi, 6);
+double RoundedE = Math.Round(e, 6);
+Console.WriteLine($"{RoundedE} {RoundedPi}");
+
+
+
+
