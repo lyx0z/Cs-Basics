328dbbfc227d0c1ea8ef0c01151ebd010d9567f4
lyx0z <mykyta.kucher@digitecgalaxus.ch>
2026-08-12 17:49:41 +0200

    06-OOP
    added Bankaccount class

---
+++ 06-OOP/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿namespace _06_OOP;
+
+public class BankAccount
+{
+    private int balance = 0;
+
+    static void ChooseOperation()
+    {
+        Console.WriteLine("Choose your Operation: "); 
+        Console.WriteLine("deposit 1; withdraw 2; transfer 3. ");
+        int operation = int.Parse(Console.ReadLine());
+        if (operation == 1)
+        {
+            Console.WriteLine("Your balance is:", BankAccount.balance);
+            Console.WriteLine("How much would you like to deposit?:");
+            int deposit = int.Parse(Console.WriteLine());
+            balance = balance + deposit;
+            Console.WriteLine("Your new balance is:", balance);
+        }
+        
+
+    }
+}
---
+++ 06-OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}    
