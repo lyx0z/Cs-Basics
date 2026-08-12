e2702d199270a04ca2f5a4cdd12dd569feff2617
lyx0z <mykyta.kucher@digitecgalaxus.ch>
2026-08-12 18:33:22 +0200

    06-OOP
    updated console looks and added withdrawal

--- 06-OOP/BankAccount.cs
+++ 06-OOP/BankAccount.cs
@@ -3,21 +3,31 @@
 public class BankAccount
 {
     private int balance = 0;
-
-    static void ChooseOperation()
+    public void Deposit(int amount)
     {
-        Console.WriteLine("Choose your Operation: "); 
-        Console.WriteLine("deposit 1; withdraw 2; transfer 3. ");
-        int operation = int.Parse(Console.ReadLine());
-        if (operation == 1)
+        balance = balance + amount;
+    }
+    
+    public void ShowBalance()
+    {
+        Console.WriteLine($"Your balance is: {balance}");
+    }
+
+    public bool Withdraw(int amount)
+    {
+        if (amount > balance)
         {
-            Console.WriteLine("Your balance is:", BankAccount.balance);
-            Console.WriteLine("How much would you like to deposit?:");
-            int deposit = int.Parse(Console.WriteLine());
-            balance = balance + deposit;
-            Console.WriteLine("Your new balance is:", balance);
+            Console.WriteLine("Not enough funds :(");
+            Console.ReadKey();
+            return false;
         }
         
-
+        else
+        {
+            balance = balance - amount;
+            return true;
+        }
+        
     }
+
 }
--- 06-OOP/Program.cs
+++ 06-OOP/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace _06_OOP
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using _06_OOP;
 
+BankAccount account = new BankAccount();
+
+bool running = true;
+
+while (running)
+{
+    Console.Clear();
+    Console.WriteLine();
+    Console.WriteLine("TerminalBANK");
+    Console.WriteLine("1. Show balance");
+    Console.WriteLine("2. Deposit");
+    Console.WriteLine("3. Withdraw");
+    Console.WriteLine("4. Exit");
+    Console.Write("Choose an option: ");
+
+    int choice = int.Parse(Console.ReadLine());
+
+    if (choice == 1)
+    {
+        account.ShowBalance();
+        Console.ReadKey();
+    }
+    else if (choice == 2)
+    {
+        Console.Write("How much do you want to deposit? ");
+        int amount = int.Parse(Console.ReadLine());
+
+        account.Deposit(amount);
+
+        Console.WriteLine("Deposit successful!");
+        Console.ReadKey();
+    }
+    
+    else if (choice == 3)
+    {
+        Console.Write("How much do you want to withdraw? ");
+        int amount = int.Parse(Console.ReadLine());
+        
+        if (account.Withdraw(amount))
+        {
+            Console.WriteLine("Withdrawal successful!");
+            Console.ReadKey();
         }
     }
-}    
+
+    else if (choice == 4)
+    {
+        running = false;
+        Console.WriteLine("Goodbye!");
+        Console.ReadKey();
+    }
+    else
+    {
+        Console.WriteLine("Invalid option.");
+        Console.ReadKey();
+    }
+    
+}
