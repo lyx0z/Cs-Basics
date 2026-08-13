f626860d7ff97656a50c25fc0350064a315649f9
lyx0z <mykyta.kucher@digitecgalaxus.ch>
2026-08-13 17:25:46 +0200

    Fizzbuzz

---
+++ Extra-Fizzbuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Extra_Fizzbuzz;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Whats your number?");
+        int n = int.Parse(Console.ReadLine());
+        int numberOfAttemps = 1;
+
+        while (numberOfAttemps <= n)
+        {
+            if (numberOfAttemps % 3 == 0 && numberOfAttemps % 5 == 0)
+            {
+                Console.WriteLine("Fizzbuzz");
+            }
+            
+            else if(numberOfAttemps % 3 == 0)
+            {
+              Console.WriteLine("Fizz");
+            }
+            
+            else if(numberOfAttemps % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+
+            else
+            {
+                Console.WriteLine(numberOfAttemps);
+            }
+
+            numberOfAttemps++;
+
+        }
+
+    }
+}
+
