47676ca786e362f88bee1c91e4f35862732e5604
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-08-17 10:01:29 +0200

    07-Arrays
    task 3 and 4

--- 07-Arrays/Program.cs
+++ 07-Arrays/Program.cs
@@ -47,9 +47,58 @@ namespace Operatoren
             }
             
             
+            //Schreibe eine Methode, die jedes Element eines Arrays mit Ganzzahlen mit sich selbst multipliziert und als Array in der gleichen Reihenfolge zurückgibt. Benutze dafür foreach.
+            static int[] ArrayMultiplication(int[] arr)
+            {
+                var multiplicated = new int[arr.Length];
+                var index = 0; 
 
+                foreach (var number in arr)
+                {
+                    multiplicated[index] = number * number;
+                    index++;
+                }
 
+                return multiplicated;
+            }
 
+            int[] numbers = [1, 2, 3, 4, 5];
+            var multiplicated = ArrayMultiplication(numbers);
+
+            foreach (var number in multiplicated)
+            {
+                Console.WriteLine(number);
+            }
+            //Schreibe eine eigene Sortiermethode (also nicht Sort() benutzen) für einen Ganzzahligen-Array.
+            static int[] ArraySort(int[] arr)
+            {
+                var sorted = new int[arr.Length];
+                int temp = 0;
+                for (int i = 0; i <= arr.Length-1; i++)  
+                {  
+                    for (int j = i+1; j < arr.Length; j++)  
+                    {  
+                        if (arr[i] > arr[j])  
+                        {  
+                            temp = arr[i];  
+                            arr[i] = arr[j];  
+                            arr[j] = temp;  
+                        }  
+                    }  
+                }
+
+                return arr;
+            }
+            int[] arrayToSort = [5, 4, 3, 2, 1, 7, 8, 6, 9];
+            var sorted = ArraySort(arrayToSort);
+            foreach (var item in sorted)  
+            {  
+                Console.WriteLine(item);  
+            }  
+            
+            //Schreibe eine Methode, mit der du einem Array von Ganzzahlen ein Element hinzufügen kannst.
+            
+            
         }
     }
 }
